36c3f6a718292204587e12e2e9b7b4456d1b400f
Murali Krishna Marimekala <murali.marimekala@gmail.com>
2017-11-11 11:31:45 -0800

    Adding few more c source
    Signed-off-by: Murali Krishna Marimekala <murali.marimekala@gmail.com>

--- ll_tail_3.c
+++ ll_tail_3.c
@@ -11,7 +11,7 @@ void push(struct node** headRef, int data) {
 	newNode=(struct node*)malloc(sizeof(struct node));
 	newNode->data=data;   
 	printf("%s:%d: Pushed value is %d\n",__FUNCTION__,__LINE__,newNode->data);
-   newNode->next=*headRef; //Link next
+    newNode->next=*headRef; //Link next
 	*headRef=newNode; //Link head
 }
 
@@ -23,31 +23,26 @@ void printList(struct node* head) {
 }
 
 void BuildWithSpecialCase(struct node** headRef) {
-//void BuildWithSpecialCase(struct node* head) {
 	struct node* tail;
 	int i;
 	tail=*headRef;
-	for(i=6;i<10;i++) {
-		push(&((*tail)->next),i);
+	for(i=1;i<10;i++) {
+		push(&(tail->next),i);
 		tail = tail->next; //Advance the next pointer
 	}
 	printf("\n Printing header : start\n");	
-   printList(head);
+    printList(tail);
 	printf("\n Printing header : stop\n");	
 }
 int main() {
 	struct node* head=NULL;
 	head=(struct node*)malloc(sizeof(struct node));
-//	struct node* head1=NULL;
-//	head1=(struct node*)malloc(sizeof(struct node));
 	push(&head,1);
 	push(&head,2);
 	push(&head,3);
 	push(&head,4);
 	push(&head,5);
-//   printList(head);
-//	printf("-------------------\n");
 	BuildWithSpecialCase(&head);
-   printList(head);
+    printList(head);
 	return 0;
 }
